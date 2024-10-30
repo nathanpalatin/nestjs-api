11d7cb76ee2f10a1f328a6124dd331424e42fa83
Nathan Palatin <nath.palatin@gmail.com>
2024-10-30 14:06:10 -0300

    up: deploy fly service.

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
 	const configService = app.get(EnvService)
 	const port = configService.get('PORT')
 
-	await app.listen(port)
+	await app.listen(port, '0.0.0.0')
 }
 bootstrap()
