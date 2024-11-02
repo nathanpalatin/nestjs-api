b69ac6c87fbf682c3405527f4c6ba55f1aad611c
Nathan Palatin <nath.palatin@gmail.com>
2024-11-02 09:47:14 -0300

    chore: add seed prisma.

---
+++ prisma/seed.ts
@@ -0,0 +1,19 @@
+import { faker } from '@faker-js/faker'
+import { PrismaClient } from '@prisma/client'
+import { hash } from 'bcryptjs'
+
+const prisma = new PrismaClient()
+
+async function seed() {
+	await prisma.user.create({
+		data: {
+			name: faker.person.fullName(),
+			email: faker.internet.email().toLocaleLowerCase(),
+			password: await hash('123456', 1)
+		}
+	})
+}
+
+seed().then(() => {
+	console.log('Database seeded!')
+})
